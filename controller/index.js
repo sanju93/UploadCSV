f6ab029b65805d29b84b1fb6b2ed58cd6ba76992
sanjay <sanjay139747@gmail.com>
2023-03-26 23:28:18 +0530

    deploy1

--- controller/index.js
+++ controller/index.js
@@ -15,13 +15,13 @@ module.exports.upload = async (req,res) => {
     var index = filename.indexOf('.');
     filename = filename.substring(0,index) + Date.now() + filename.substring(index,filename.length);
 
-    req.files.data.mv(__dirname+'/../uploads/files/'+filename,async (err) => {
+    req.files.data.mv(__dirname+'/../tmp/'+filename,async (err) => {
         if (err) {
             console.log(err);
             return;
         }else{
             
-        const filePath = __dirname + '/../uploads/files/' + filename;
+        const filePath = __dirname + '/../tmp/' + filename;
        csv()
         .fromFile(filePath)
         .then(async (results) => { 
